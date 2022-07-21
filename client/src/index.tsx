e44b30e763aff45fe6630c3537dd7d114a46d7b1
Merkie <merkie@post.com>
2022-07-21 15:23:10 -0700

    migrate to solid start

--- client/src/index.tsx
+++
@@ -1,7 +0,0 @@
-/* @refresh reload */
-import { render } from 'solid-js/web';
-
-import './styles/index.css';
-import App from './pages/App';
-
-render(() => <App />, document.getElementById('root') as HTMLElement);
